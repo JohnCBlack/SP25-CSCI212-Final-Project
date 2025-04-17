f564ea38728823de4ecac2565b4630faa3e6d3f9
John Black <ashblack714@gmail.com>
2025-04-17 09:16:36 -0400

    REMOVED geolocator function and API call Swapped to instead ping zipcode with weatherapi Added some better notes

--- weatherAPICall.java
+++ weatherAPICall.java
@@ -7,24 +7,25 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class weatherAPICall {
-    //private static final String baseURL = "https://api.openweathermap.org/data/3.0/onecall?lat=";
-    //private static String apiKey = System.getenv("WEATHER_API_KEY");
-
+    private static final String weatherApiKey = "";
 
     public static void main(String[] args) {
-        //String[2] = [lat, lon]
+        /* Format
+        String[2] = [lat, lon]
+        */
 
-        String[] location = getLocation();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the your zip code: ");
+        String zipCode = sc.next();
 
-        //System.out.println(location[0] + " " + location[1]);
-        getWeather(location[0], location[1]);
+        getWeather(zipCode);
     }
 
-    public static void getWeather(String lat, String lon) {
+    public static void getWeather(String zipcode) {
         final String baseURL= "https://api.weatherapi.com/v1/current.json?key=";
 
         //http://api.weatherapi.com/v1/current.json?key=e0687018d39a48d1a25130036251704&q=46032&aqi=no
-        String urlStr = String.format("%s%s&q=%s,%s&aqi=no", baseURL, weatherApiKey, lat, lon);
+        String urlStr = String.format("%s%s&q=%s&aqi=no", baseURL, weatherApiKey, zipcode);
 
         try {
             URL url = new URI(urlStr).toURL();
@@ -54,48 +55,5 @@ public class weatherAPICall {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-    }
-
-    public static String[] getLocation() {
-        String[] location = new String[2];
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the your zip code: ");
-        String zipCode = sc.next();
-
-        //http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
-        String urlStr = String.format("https://api.openweathermap.org/geo/1.0/zip?zip=%s,US&appid=%s", zipCode, geoApiKey);
-
-        try {
-            URL url = new URI(urlStr).toURL();
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-
-            int responseCode = connection.getResponseCode();
-
-            if (responseCode == 200) {
-                Scanner scanner = new Scanner(url.openStream());
-                StringBuilder informationString = new StringBuilder();
-
-                while (scanner.hasNext()) {
-                    informationString.append(scanner.nextLine());
-                }
-
-                JSONParser parser = new JSONParser();
-                JSONObject jsonObject = (JSONObject) parser.parse(informationString.toString());
-                location[0] = jsonObject.get("lat").toString();
-                location[1] = jsonObject.get("lon").toString();
-
-            } else {
-                System.out.println("Response Code: " + responseCode);
-                System.out.println("Error in Geolocation API call");
-            }
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        return location;
     }
 }
