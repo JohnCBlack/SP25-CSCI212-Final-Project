5fda603788c6065da3731a118f608f641542b8d0
john <johncb@iu.edu>
2025-04-17 10:59:24 -0400

    Added setting API key from environmental variables

--- weatherAPICall.java
+++ weatherAPICall.java
@@ -10,11 +10,6 @@ import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.Scanner;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Properties;
-
 public class weatherAPICall {
     private static String weatherApiKey;
 
