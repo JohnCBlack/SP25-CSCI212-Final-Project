46a6b9a4d4ceb81dd3ee819d702894e36128d2bb
John Black <ashblack714@gmail.com>
2025-04-17 23:16:42 -0400

    Implemented get date

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
-import java.time.Instant;
-
 public class Main {
-    static Instant today = Instant.now();
-
     public static void main(String[] args) {
-
+        stockAPICall stock = new stockAPICall();
     }
 }
