cc9a94e4326d7a7afbc3af8d0cf30bdd44e3d153
John Black <ashblack714@gmail.com>
2025-04-23 23:10:01 -0400

    Added easy functions for testing purposes

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        //weatherAPICall weather = new weatherAPICall();
+        weatherAPICall weather = new weatherAPICall();
+
+        //FOR TESTING PURPOSES
+        System.out.println(
+                "Weather Report:\n" +
+                        "Condition: " + weather.condition + "\n" +
+                        "Current Temperature: " + weather.currentTemp + "°F\n" +
+                        "Maximum Temperature: " + weather.maxTemp + "°F\n" +
+                        "Minimum Temperature: " + weather.minTemp + "°F\n" +
+                        "Chance of Rain: " + weather.changeOfRain + "%"
+        );
+
         stockAPICall stock = new stockAPICall();
 
         //FOR TESTING PURPOSES
