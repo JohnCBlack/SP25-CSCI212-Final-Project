6d2c4fa20b5e86d7718806e7c3e6167450cb1692
John Black <ashblack714@gmail.com>
2025-04-23 22:53:48 -0400

    added test function

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         //weatherAPICall weather = new weatherAPICall();
         stockAPICall stock = new stockAPICall();
+
+        //FOR TESTING PURPOSES
+        System.out.printf("%f, %f", stock.currentPrice, stock.percentChange);
     }
 }
