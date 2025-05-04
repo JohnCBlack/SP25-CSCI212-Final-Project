3914152c8efdb58db871675c7c4fbbe47dfffa68
John Black <ashblack714@gmail.com>
2025-05-04 15:14:57 -0400

    Removed unnecessary code

--- src/main/java/com/csci212/finalproject/NewsAPICall.java
+++ src/main/java/com/csci212/finalproject/NewsAPICall.java
@@ -1,6 +1,5 @@
 package com.csci212.finalproject;
 
-import javafx.scene.control.TextArea;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -13,10 +12,7 @@ import java.util.Map;
 
 
 public class NewsAPICall extends APICall{
-    private String keyWord;
-    private String category;
-    private String country;
-    private String language;
+    private String keyWord,category,country,language;
 
 
     //Hashmap for language to its code
@@ -38,76 +34,11 @@ public class NewsAPICall extends APICall{
         languageMap.put("Chinese","zh");
     }
 
-    //Hashmap of all countries that are offered by the API
-    public static final Map<String, String> countryMap = new HashMap<>();
-    static {
-        countryMap.put("Argentina", "ar");
-        countryMap.put("Australia", "au");
-        countryMap.put("Austria", "at");
-        countryMap.put("Belgium", "be");
-        countryMap.put("Brazil","br");
-        countryMap.put("Canada", "ca");
-        countryMap.put("China","cn");
-        countryMap.put("Colombia", "co");
-        countryMap.put("Cuba","cu");
-        countryMap.put("Czech Republic", "cz");
-        countryMap.put("Egypt","eg");
-        countryMap.put("France","fr");
-        countryMap.put("Germany", "de");
-        countryMap.put("Greece", "gr");
-        countryMap.put("Hong Kong", "hk");
-        countryMap.put("Hungary", "hu");
-        countryMap.put("India", "in");
-        countryMap.put("Indonesia", "id");
-        countryMap.put("Ireland", "ir");
-        countryMap.put("Israel","il");
-        countryMap.put("Italy","it");
-        countryMap.put("Japan", "jp");
-        countryMap.put("Latvia", "lv");
-        countryMap.put("Lithuania", "lt");
-        countryMap.put("Malaysia","my");
-        countryMap.put("Mexico", "mx");
-        countryMap.put("Morocco", "ma");
-        countryMap.put("Netherlands","nl");
-        countryMap.put("New Zealand", "nz");
-        countryMap.put("Nigeria","ng");
-        countryMap.put("Norway","no");
-        countryMap.put("Philippines","ph");
-        countryMap.put("Poland", "pl");
-        countryMap.put("Portugal", "pt");
-        countryMap.put("Romania", "ro");
-        countryMap.put("Russia","ru");
-        countryMap.put("Saudi Arabia", "sa");
-        countryMap.put("Serbia","rs");
-        countryMap.put("Singapore","sg");
-        countryMap.put("Slovakia","sl");
-        countryMap.put("Slovenia", "si");
-        countryMap.put("South Africa","za");
-        countryMap.put("South Korea","kr");
-        countryMap.put("Sweden", "se");
-        countryMap.put("Switzerland", "ch");
-        countryMap.put("Taiwan","tw");
-        countryMap.put("Thailand","th");
-        countryMap.put("Turkey","tr");
-        countryMap.put("UAE","ae");
-        countryMap.put("Ukraine","ua");
-        countryMap.put("United Kingdom","gb");
-        countryMap.put("United States","us");
-        countryMap.put("Venezuela","ve");
-    }
-
 
     //Possibly a dropdown to give a list of sources
     //GET https://newsapi.org/v2/everything?q=Apple&from=2025-04-18&sortBy=popularity&apiKey=API_KEY
     // GET https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY
-    /**
-    public NewsAPICall(String source){
-        this.source = source;
-        setApiKey("NEWS_API_KEY");
-    }
-    */
 
-    //type 1 for news cite. Type 2 for general country
     public void getNewsHeadline(String category, String country) {
         this.category = category;
         this.country = country;
@@ -121,7 +52,6 @@ public class NewsAPICall extends APICall{
 
         processData(urlStr);
     }
-
 
     public void getNewsKeyWord(String keyWord, String language) {
         this.keyWord = keyWord;
