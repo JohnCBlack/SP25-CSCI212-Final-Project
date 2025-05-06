3db4e5c6862d0cc454089e26d51ab22a3fdb9ca1
John Black <ashblack714@gmail.com>
2025-05-06 10:36:50 -0400

    removed loging function as other members could not get it to run

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.csci212.finalproject {
     requires javafx.fxml;
     requires json.simple;
     requires java.desktop;
-    requires org.slf4j;
 
 
     opens com.csci212.finalproject to javafx.fxml;
